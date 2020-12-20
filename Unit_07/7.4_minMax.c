78c3a311686f477f70e9fc03f34386d6f0ac0164
zhunhe <xuzhunhe@gmail.com>
2020-12-21 06:03:37 +0900

    Create

--- Unit_07/7.4_minMax.c
+++ Unit_07/7.4_minMax.c
@@ -3,30 +3,28 @@
 
 void min(void)
 {
-    char num1 = CHAR_MIN;          // char의 최솟값
-    short num2 = SHRT_MIN;         // short의 최솟값
-    int num3 = INT_MIN;            // int의 최솟값
-    long num4 = LONG_MIN;          // long의 최솟값
-    long long num5 = LLONG_MIN;    // long long의 최솟값
+    char num1 = CHAR_MIN;
+    short num2 = SHRT_MIN;
+    int num3 = INT_MIN;
+    long num4 = LONG_MIN;
+    long long num5 = LLONG_MIN;
 
-    // char, short, int는 %d로 출력하고 long은 %ld로 출력, long long은 %lld로 출력
     printf("%d %d %d %ld %lld\n", num1, num2, num3, num4, num5);
-    // -128 -32768 -2147483648 -2147483648 -9223372036854775808
 }
 
 void max(void)
 {
-    char num1 = CHAR_MAX + 1;
-    short num2 = SHRT_MAX + 1;
-    int num3 = INT_MAX + 1;
-    long long num4 = LLONG_MAX + 1;
+    char num1 = CHAR_MAX;
+    short num2 = SHRT_MAX;
+    int num3 = INT_MAX;
+    long long num4 = LLONG_MAX;
 
     printf("%d %d %d %lld\n", num1, num2, num3, num4);
 
-    unsigned char num5 = UCHAR_MAX + 1;
-    unsigned short num6 = USHRT_MAX + 1;
-    unsigned int num7 = UINT_MAX + 1;
-    unsigned long long num8 = ULLONG_MAX + 1;
+    unsigned char num5 = UCHAR_MAX;
+    unsigned short num6 = USHRT_MAX;
+    unsigned int num7 = UINT_MAX;
+    unsigned long long num8 = ULLONG_MAX;
 
     printf("%u %u %u %llu\n", num5, num6, num7, num8);
 }
