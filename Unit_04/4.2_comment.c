47ec1de2c3cad94ff7cadaac05a6119c3a13c81d
zhunhe <xuzhunhe@gmail.com>
2020-12-21 01:46:30 +0900

    Create

---
+++ Unit_04/4.2_comment.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(void)
+{
+    // Hello, world! 출력
+    printf("Hello, world!\n");
+
+    //printf("Hello, world!\n");
+
+    int a = 1 + 2; // 더하기
+
+    printf("Hello, world!\n");//printf("1234567890");
+
+    /*
+    printf("Hello, world!\n");
+    printf("1234567890");
+    */
+
+    printf("Hello\n" /* 안녕하세요 */);
+
+    return 0;
+}
