9fa67796d8fd99d19be8cb8d53a82b8a150b996f
zhunhe <xuzhunhe@gmail.com>
2020-12-21 07:41:13 +0900

    Add Example

--- Unit_10/10.3_literalSuffix.c
+++ Unit_10/10.3_literalSuffix.c
@@ -11,5 +11,10 @@ int main(void)
     printf("%lu\n", 10UL);
     printf("%llu\n", 1234567890123456789ULL);
 
+    printf("0%lo\n", 017L);
+    printf("0%lo\n", 017UL);
+    printf("0x%lX\n", 0x7FFFFFL);
+    printf("0x%lX\n", 0xFFFFFFFFUL);
+
     return 0;
 }
