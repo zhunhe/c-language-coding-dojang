b4e40fa342e71c92f393e18104fb0fe879380175
zhunhe <xuzhunhe@gmail.com>
2020-12-21 07:19:35 +0900

    Add Example

--- Unit_09/9.1_char.c
+++ Unit_09/9.1_char.c
@@ -14,5 +14,11 @@ int main(void)
     printf("%c, %d\n", c3, c3);
     printf("%c, %d\n", c4, c4);
 
+    char c5 = 0x61;
+    char c6 = 0x62;
+
+    printf("%c, %d\n", c5, c5);
+    printf("%c, %d\n", c6, c6);
+
     return 0;
 }
