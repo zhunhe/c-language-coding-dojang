cf1d87089ce406428207ab63546284c544b928e5
zhunhe <xuzhunhe@gmail.com>
2020-12-21 07:36:09 +0900

    Create

---
+++ Unit_10/10.1_literal.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 10);
+    printf("%f\n", 0.1f);
+    printf("%c\n", 'a');
+    printf("%s\n", "Hello, world!");
+
+    printf("%d\n", 19);
+    printf("0%o\n", 017);
+    printf("0x%X\n", 0x1F);
+
+    return 0;
+}
