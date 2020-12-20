59096bd350650b8dabfc948dbb7122526383044d
zhunhe <xuzhunhe@gmail.com>
2020-12-21 07:38:21 +0900

    Create

---
+++ Unit_10/10.2_const.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    const int con1 = 1;
+    const float con2 = 0.1f;
+    const char con3 = 'a';
+
+    printf("%d %f %c\n", con1, con2, con3);
+
+    return 0;
+}
