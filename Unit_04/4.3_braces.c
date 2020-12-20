da16a275678e4e3218ea6502c3f85e93b0fa6a8b
zhunhe <xuzhunhe@gmail.com>
2020-12-21 01:56:10 +0900

    Create

---
+++ Unit_04/4.3_braces.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+// 구조체(structure), 공용체(union), 열거형(enum)
+struct Hello {
+    int a;
+    int b;
+};
+
+// 함수(function), 반복문(loop), 조건문(if-else statement) 등
+int main(void)
+{
+    return 0;
+}
---
+++ Unit_04/4.4_indent.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(void)
+{
+////return 0;   // 일반적으로 1 tab = 4 space를 기준으로 함
+}
