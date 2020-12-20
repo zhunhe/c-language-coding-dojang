1f6d53ad16267d31cbc8ab4fb8f397becd68082d
zhunhe <xuzhunhe@gmail.com>
2020-12-21 06:32:17 +0900

    Create

---
+++ Unit_08/8.2_sizeofRealNumber.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float num1 = 0.0f;
+    double num2 = 0.0;
+    long double num3 = 0.0l;
+
+    printf("float: %d, double: %d, long double: %d\n", sizeof(num1), sizeof(num2), sizeof(num3));
+
+    return 0;
+}
