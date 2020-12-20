88681bef8a85085ca7bc10093a03e838be2c2e1a
zhunhe <xuzhunhe@gmail.com>
2020-12-21 07:22:36 +0900

    Create

---
+++ Unit_09/9.2_charPlus.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(void)
+{
+    printf("%c %d\n", 'a' + 1, 'a' + 1);
+    printf("%c %d\n", 97 + 1, 97 + 1);
+
+    return 0;
+}
---
+++ Unit_09/9.3_linefeed.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(void)
+{
+    char c1 = 'a';
+    char c2 = 'b';
+    char lineFeed = '\n';
+
+    printf("%c%c%c%c", c1, lineFeed, c2, lineFeed);
+
+    return 0;
+}
