30bd0d9f27d188944ebdb0136ddbe92f54e6b867
zhunhe <xuzhunhe@gmail.com>
2020-12-21 06:27:39 +0900

    Create

---
+++ Unit_08/8.1_realNumber.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void)
+{
+    float num1 = 0.1f;
+
+    double num2 = 3867.215820;
+
+    long double num3 = 9.327513l;
+
+    printf("%f %f %Lf\n", num1, num2, num3);
+
+    return 0;
+}
