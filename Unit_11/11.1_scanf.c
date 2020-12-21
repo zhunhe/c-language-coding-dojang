2a11c2dcbeaad1811c5677cf2e10bfb24167de55
zhunhe <xuzhunhe@gmail.com>
2020-12-21 17:56:17 +0900

    Create

---
+++ Unit_11/11.1_scanf.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)
+{
+    int num1;
+
+    printf("Input integer: ");
+    scanf("%d", &num1);
+
+    printf("%d\n", num1);
+
+    return 0;
+}
