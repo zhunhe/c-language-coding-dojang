8d303b84bbf77cf4bb8758612369f1aedf09e885
zhunhe <xuzhunhe@gmail.com>
2020-12-21 21:18:03 +0900

    Create

---
+++ Unit_19/19.1_ifElseif.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 20;
+
+    if (num1 == 10)
+        printf("num1 is 10\n");
+    else if (num1 == 20)
+        printf("num1 is 20\n");
+
+    return 0;
+}
---
+++ Unit_19/19.2_ifElseifElse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 30;
+
+    if (num1 == 10)
+        printf("num1 is 10\n");
+    else if (num1 == 20)
+        printf("num1 is 20\n");
+    else
+        printf("num1 is not 10 and 20\n");
+
+    return 0;
+}
