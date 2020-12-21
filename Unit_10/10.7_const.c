4476012c70916a21d1f099d3f54d468009960153
zhunhe <xuzhunhe@gmail.com>
2020-12-21 17:55:02 +0900

    Create

---
+++ Unit_10/10.7_const.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    const unsigned long long con1 = 10;
+    const double con2 = 3.2;
+    const char con3 = 't';
+
+    printf("%llu %f %c\n", con1, con2, con3);
+
+    return 0;
+}
