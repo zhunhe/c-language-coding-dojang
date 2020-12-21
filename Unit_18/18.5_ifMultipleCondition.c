4c8e00ce0193c4c031b467d0ffe76b7f6ddcc3e2
zhunhe <xuzhunhe@gmail.com>
2020-12-21 21:02:44 +0900

    Create

---
+++ Unit_18/18.5_ifMultipleCondition.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int num1 = 10;
+    int num2 = 20;
+
+    if (num1 == 10 && num2 == 20)    // num1이 10이면서 num2이 20일 때
+        printf("OK\n");
+    else
+        printf("NG\n");
+
+    return 0;
+}
